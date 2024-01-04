d057ea32e1f7dfb03a1296df8ee5706e19df93aa
Ronicola97 <loornico15@gmail.com>
2024-01-03 21:07:05 -0500

    fixing diagnostico creation

--- machinelearning/serializers.py
+++ machinelearning/serializers.py
@@ -6,4 +6,4 @@ class DiagnosticoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Diagnostico
         fields = ('id','enfermedad','procentaje','ficha','fecha_analisis')
-        read_only_fields = ('fecha_analisis')
+        read_only_fields = ('fecha_analisis',)
